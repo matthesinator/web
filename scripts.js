a07281150bf811199622d2907f11bc37ae2474f2
Max <brosziomax@posteo.de>
2019-11-21 18:25:00 +0100

    Buttons zum zuklappen der Paragraphen eingefügt. Noch kein CSS dafür

---
+++ scripts.js
@@ -0,0 +1,13 @@
+var btn_zuklappbar = document.getElementsByClassName("collapsible");
+for(var i = 0; i < btn_zuklappbar.length; i++) {
+  btn_zuklappbar[i].addEventListener("click", function () {
+    lern_inhalt = this.nextElementSibling;
+    console.log(this);
+    console.log(lern_inhalt);
+    if(lern_inhalt.classList.contains("hidden")){ 
+      lern_inhalt.classList.remove("hidden");
+    } else {
+      lern_inhalt.classList.add("hidden");
+    }
+  });
+} 
