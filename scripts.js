932b38c62260ed8b5e104ba7e70a4e1d5ea47bd8
Matthes <matthes@mwuerbs-l380.speedport.ip>
2019-11-21 19:09:37 +0100

    Cookieleiste ist da und mit js versehen

--- scripts.js
+++ scripts.js
@@ -1,13 +1,18 @@
 var btn_zuklappbar = document.getElementsByClassName("collapsible");
-for(var i = 0; i < btn_zuklappbar.length; i++) {
+for (var i = 0; i < btn_zuklappbar.length; i++) {
   btn_zuklappbar[i].addEventListener("click", function () {
     lern_inhalt = this.nextElementSibling;
     console.log(this);
     console.log(lern_inhalt);
-    if(lern_inhalt.classList.contains("hidden")){ 
+    if (lern_inhalt.classList.contains("hidden")) {
       lern_inhalt.classList.remove("hidden");
     } else {
       lern_inhalt.classList.add("hidden");
     }
   });
-} 
+}
+
+var cookie = document.getElementById("cookie-button");
+cookie.addEventListener("click", function() {
+  document.getElementById("cookie").classList.add("hidden");
+});
