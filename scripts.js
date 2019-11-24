75b664121e843e14c3cc3b2641d152d61887a80a
Matthes <m.wuerbs@gmail.com>
2019-11-24 15:37:45 +0100

    Zeiger bei Hover geändert, kompletten Name klickbar gemacht

--- scripts.js
+++ scripts.js
@@ -1,16 +1,17 @@
-var btn_zuklappbar = document.getElementsByClassName("pfeil-d");
+var btn_zuklappbar = document.getElementsByClassName("hider");
 for (var i = 0; i < btn_zuklappbar.length; i++) {
   btn_zuklappbar[i].addEventListener("click", function () {
     console.log(this);
-    if(this.classList.contains("pfeil-d")){
-      this.classList.remove("pfeil-d");
-      this.classList.add("pfeil-r");
+    pfeil = this.firstChild
+    if(pfeil.classList.contains("pfeil-d")){
+      pfeil.classList.remove("pfeil-d");
+      pfeil.classList.add("pfeil-r");
     } else {
-      this.classList.remove("pfeil-r");
-      this.classList.add("pfeil-d");
+      pfeil.classList.remove("pfeil-r");
+      pfeil.classList.add("pfeil-d");
     }
     console.log(this);
-    lern_inhalt = this.parentNode.nextElementSibling;
+    lern_inhalt = this.nextElementSibling;
     if (lern_inhalt.classList.contains("hidden")) {
       lern_inhalt.classList.remove("hidden");
     } else {
