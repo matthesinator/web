581594b3f895becc0275282bee61d2da9a644b7e
Max <brosziomax@posteo.de>
2019-11-27 19:14:19 +0100

    Script aufgeräumt

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 var btn_zuklappbar = document.getElementsByClassName("hider");
 for (var i = 0; i < btn_zuklappbar.length; i++) {
   btn_zuklappbar[i].addEventListener("click", function () {
-    console.log(this);
     pfeil = this.firstChild
     if(pfeil.classList.contains("pfeil-d")){
       pfeil.classList.remove("pfeil-d");
@@ -10,7 +9,6 @@ for (var i = 0; i < btn_zuklappbar.length; i++) {
       pfeil.classList.remove("pfeil-r");
       pfeil.classList.add("pfeil-d");
     }
-    console.log(this);
     lern_inhalt = this.nextElementSibling;
     if (lern_inhalt.classList.contains("hidden")) {
       lern_inhalt.classList.remove("hidden");
@@ -20,7 +18,3 @@ for (var i = 0; i < btn_zuklappbar.length; i++) {
   });
 }
 
-var cookie = document.getElementById("cookie-button");
-cookie.addEventListener("click", function() {
-  document.getElementById("cookie").classList.add("hidden");
-});
